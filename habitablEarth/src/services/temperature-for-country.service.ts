738f024e4745e49e27f1455485c72b6341acd5e6
Just-Simon-Dev <39631698+Just-Simon-Dev@users.noreply.github.com>
2023-03-08 09:17:03 +0100

    bug fixies

--- habitablEarth/src/services/temperature-for-country.service.ts
+++ habitablEarth/src/services/temperature-for-country.service.ts
@@ -9,7 +9,7 @@ export class TemperatureForCountryService {
 	private url: string | undefined
 	constructor(private http: HttpClient) { }
 	getTemperatures(end_date: string | null, country: string){
-		this.url = "http://habitable-earth:5000/api/countries"
+		this.url = "http://habitable-earth.eu:5000/api/countries"
 		return this.http.get(`${this.url}?end_date=${end_date}&country=${country}`)
 	}
 }
