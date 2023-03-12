0762dbc7a1d47147ae3c5dfae5462bfea0eaf026
Just-Simon-Dev <39631698+Just-Simon-Dev@users.noreply.github.com>
2023-03-12 20:20:10 +0100

    updates

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ def get_countries():
 	return jsonify(temperatures)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
 # @app.route('/api/cities', methods=["GET"])
 # def get_cities():
